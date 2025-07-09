1c37fc5626694cfd019b57aa398800a0630f4ec9
Matatcoding <matthew_whitlock@comcast.net>
2025-07-08 23:04:51 -0400

    Finished the code and did a little css

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-const displayResults = (parties) => {
-  $app = document.querySelector("#app");
+let parties = [];
+let selectedParty;
+
+const getMoreInfo = async (id) => {
+  try {
+    const response = await fetch(
+      `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2505-ftb-ct-web-pt/events/${id}`
+    );
+    const data = await response.json();
+    console.log(data.data);
+    selectedParty = data.data;
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const header = () => {
   $header = document.createElement("header");
   $title = document.createElement("h1");
   $title.textContent = "Party Planner";
   $header.append($title);
-  $app.append($header);
+  return $header;
+};
 
-  $upcomingDiv = document.createElement("div");
+const displayResults = () => {
+  $upcomingSec = document.createElement("section");
   $h2 = document.createElement("h2");
-  $h2.textContent = "Upcoming Events";
-  $upcomingDiv.append($h2);
-  $detailsDiv = document.createElement("div");
-  $app.append($upcomingDiv);
+  $h2.textContent = "Upcoming Parties";
+  $upcomingSec.append($h2);
 
   for (const element of parties) {
     $h3 = document.createElement("h3");
     $h3.textContent = element.name;
-    //     $img = document.createElement("img");
-    //     $img.src = element.image;
-    $button = document.createElement("button");
-    $button.textContent = "Click to get more info";
-    $button.addEventListener("click", () => getMoreInfo(element.id));
-    $upcomingDiv.append($h2);
-    $upcomingDiv.append($h3);
-    //     $div.append($img);
-    //     $div.append($button);
+    $h3.addEventListener("click", () => getMoreInfo(element.id));
+    $upcomingSec.append($h3);
   }
-  $app.append($detailsDiv);
+  return $upcomingSec;
+};
+
+const partyDetails = () => {
+  $detailsSec = document.createElement("section");
+  if (!selectedParty) {
+    $p = document.createElement("p");
+    $p.textContent = "Please select a party to learn more.";
+    $detailsSec.append($p);
+    return $detailsSec;
+  }
+  const $party = document.createElement("section");
+  $party.innerHTML = `
+      <h3>${selectedParty.name}   id: #${selectedParty.id}</h3>
+      <time>Date: ${selectedParty.date.slice(0, 10)}</time>
+      <address>Address: ${selectedParty.location}</address>
+      <p>${selectedParty.description}</p>
+    `;
+
+  return $party;
 };
 
 const fetchData = async () => {
@@ -37,14 +65,28 @@ const fetchData = async () => {
 
     const data = await response.json();
     console.log(data.data);
-    displayResults(data.data);
+    parties = data.data;
+    render();
   } catch (error) {
     console.error(error);
   }
 };
 
+const render = () => {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = "";
+
+  $app.append(header());
+
+  $content = document.createElement("div");
+  $content.classList.add("partyInfo");
+  $content.append(displayResults());
+  $content.append(partyDetails());
+
+  $app.append($content);
+};
+
 const start = () => {
-  console.log("I am in the start function");
   fetchData();
 };
 
